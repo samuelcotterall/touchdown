3c19e8e48d6d284ed24cff04dec25feb305565e2
Samuel Cotterall <samuelcotterall@gmail.com>
2011-11-02 12:11:40 +0000

    That’ll do.

--- jquery.touchdown.js
+++ jquery.touchdown.js
@@ -2,21 +2,31 @@
 * jQuery Droptouch Plugin
 */
 (function($) {
+
 	$.fn.DropTouch = function() {
-		//Link background colors 
-		var colors = new Array('4AC7ED', 'FDC015', '9F78EC', 'F25C33');
+
 		return this.each(function(i,obj) {
+
 			$this = $(this);
-			$anchors = $(obj).find("a").get();
-			$.each($anchors, function(j,ele) {
-				var randColor = Math.floor (Math.random() * colors.length);
-				$(ele).css({
-					'background-color':'#'+colors[randColor],
-					'text-decoration':'none',
-					'color':'#333333',
-					'padding':'0px 5px 0px 5px'
-				});
+
+			var options;
+			
+			for (var i = $this.find('li').length - 1; i >= 0; i--){
+				var item = $this.find('li')[i],
+					anchor = $(item).find('a'); 
+				options += '<option value="' + anchor.attr('href') + '">' + anchor.text() + '</option>';
+			};			
+			
+			$this.after('<select class="droptouch"> ' + options +'</select>');
+
+			// Event handler
+			$this.next('select').change(function(){
+				window.location = $(this).val();
 			});
+
+
 		});
+		
 	};
+	
 })(jQuery);
