2443c97343dc41cbc891817c0fe9765551f76428
Samuel Cotterall <samuelcotterall@gmail.com>
2011-11-09 14:14:50 +0000

    Added support for default titles

--- jquery.touchdown.js
+++ jquery.touchdown.js
@@ -13,19 +13,28 @@
 			
 			var listDepth = $this.parents().length,
 				anchor = $this.find('a'),
+				title = 'Select',
 				optionList;
-				
+			
+			// Create a default `<option>` for the list - If this is missing, fall back to 'Select'
+			if ($this.attr('title')) {
+				title = $this.attr('title');
+			} 
+			
+			optionList += '<option value="">' + title + '</option>';																	
+			
+			// Convert each anchor to an `<option>`
 			for (var i=0; i < anchor.length; i++) {
 				
 				var a = $(anchor[i]), 										// Current <a>
 					linkDepth = ((a.parents().length - listDepth) / 2) - 1, // Current <a>'s depth minus main list's depth divided by 2 (account for both <ul> and <li> parents) minus 1
 					indent = '';											// Reset indent
 					
-				while (linkDepth > 0){		// Append a space for each level
+				while (linkDepth > 0){										// Append a space for each level
 					indent += '\u00a0 ';
 					linkDepth--;
 				}
-					
+
 				optionList += '<option value="' + a.attr('href') + '">' + indent + a.text() + '</option>';				
 			
 			}
@@ -37,7 +46,6 @@
 			$this.next('select').change(function(){
 				window.location = $(this).val();
 			});
-
 
 		});
 		
